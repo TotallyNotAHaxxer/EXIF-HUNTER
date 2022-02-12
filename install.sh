dae4ad6e64369cdf32d7d68f4d9ad56093cecc0c
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-02-12 00:13:28 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo cpan install Getopt:Std ; sudo cpan install Image::ExifTool ; sudo cpan install Text::Table ; Term::ANSIColor ; go get github.com/bndr/gotabulate ; go get github.com/spf13/pflag
